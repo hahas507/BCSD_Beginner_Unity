ce33ae32a287eb989cee0cc24a2563ff32ee971d
hahas507 <aq12361@gmail.com>
2021-06-27 19:19:20 +0900

    19강 Start 버튼 및 씬전환
    Start 버튼 추가 및 씬 전환 기능 추가

---
+++ RunningGame/Assets/Scripts/Title/CanvasRatio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasRatio : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 DefaultSize;
+
+    [SerializeField]
+    private Camera Cam;
+
+    private void Awake()
+    {
+        if (null == Cam)
+        {
+            Debug.LogError("null == Cam");
+            return;
+        }
+
+        CanvasScaler CS = GetComponent<CanvasScaler>();
+
+        if ((DefaultSize.x / DefaultSize.y) > (Cam.pixelWidth / Cam.pixelHeight))
+        {
+            CS.matchWidthOrHeight = 0f;
+        }
+        else
+        {
+            CS.matchWidthOrHeight = 1f;
+        }
+        //Debug.Log(Cam.pixelWidth.ToString() + " " + Cam.pixelHeight.ToString());
+    }
+
+    private void Update()
+    {
+    }
+}
---
+++ RunningGame/Assets/Scripts/Title/StartButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//ipointer 이벤트 등록
+using UnityEngine.EventSystems;
+
+// 씬 관련
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //Debug.Log("씬 로드.");
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("ui 범위 밖");
+    }
+
+    public void SceneChange()
+    {
+        //Debug.Log("Test");
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
